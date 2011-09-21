d5a9455d5c4077d3f3a7c8622685d087a3312647
xiao <xiao@b55cc70a-2b87-11e0-8e4b-6b21db92dc1d>
2011-09-21 17:46:03 +0000

    git-svn-id: https://www.kr.tuwien.ac.at/krrepos/xiao/imps/drew-el/trunk/drew-el@1680 b55cc70a-2b87-11e0-8e4b-6b21db92dc1d

---
+++ src/test/java/org/semanticweb/drew/elprogram/incremental/PInstStarTest.java
@@ -0,0 +1,14 @@
+package org.semanticweb.drew.elprogram.incremental;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PInstStarTest {
+
+	@Test
+	public void test() {
+		System.out.println(PInstStar.pInstStar);
+	}
+
+}
