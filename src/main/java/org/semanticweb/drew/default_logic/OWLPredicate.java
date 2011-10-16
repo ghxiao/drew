de9d83f7b8bfb3b9528a3ff09fc483d6b14fdaaa
xiao <xiao@b55cc70a-2b87-11e0-8e4b-6b21db92dc1d>
2011-10-16 20:05:13 +0000

    update default logic model
    git-svn-id: https://www.kr.tuwien.ac.at/krrepos/xiao/imps/drew-el/trunk/drew-el@1773 b55cc70a-2b87-11e0-8e4b-6b21db92dc1d

---
+++ src/main/java/org/semanticweb/drew/default_logic/OWLPredicate.java
@@ -0,0 +1,63 @@
+package org.semanticweb.drew.default_logic;
+
+import org.semanticweb.drew.dlprogram.model.Predicate;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLLogicalEntity;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+
+/**
+ * a wrapper for OWLLogicEntity (class or role), which is used as predicate in
+ * default rules
+ * 
+ * @author xiao
+ * 
+ */
+
+public class OWLPredicate implements Predicate {
+
+	private OWLLogicalEntity logicalEntity;
+
+	public OWLPredicate(OWLLogicalEntity logicalEntity) {
+		this.logicalEntity = logicalEntity;
+	}
+
+	@Override
+	public int compareTo(Predicate o) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void setArity(int arity) {
+
+	}
+
+	@Override
+	public int getArity() {
+		if (logicalEntity instanceof OWLClass) {
+			return 1;
+		} else if (logicalEntity instanceof OWLObjectProperty) {
+			return 2;
+		} else {
+			throw new IllegalStateException();
+		}
+	}
+
+	@Override
+	public String toString() {
+		return logicalEntity.toString();
+	}
+
+	@Override
+	public OWLPredicate asOWLPredicate() {
+		return this;
+	}
+
+	public OWLLogicalEntity getLogicalEntity() {
+		return logicalEntity;
+	}
+
+	public void setLogicalEntity(OWLLogicalEntity logicalEntity) {
+		this.logicalEntity = logicalEntity;
+	}
+
+}
---
+++ src/main/java/org/semanticweb/drew/default_logic/rewritter/DefaultRuleRewriter.java
@@ -0,0 +1,66 @@
+package org.semanticweb.drew.default_logic.rewritter;
+
+import java.nio.channels.IllegalSelectorException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.semanticweb.drew.default_logic.DefaultRule;
+import org.semanticweb.drew.default_logic.OWLPredicate;
+import org.semanticweb.drew.dlprogram.model.Clause;
+import org.semanticweb.drew.dlprogram.model.Literal;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLLogicalEntity;
+
+public class DefaultRuleRewriter {
+	static final String POSTFIX_IN = "_in";
+	static final String POSTFIX_OUT = "_out";
+
+	
+	
+	// FIXME: in general, for a default ([pre : just1, ..., justm] / [concl] ),
+	// every pre/just_i, concl can be a conjunction of literals. For simplicity,
+	// we only consider the case that each conjuction only has one literal
+	List<Clause> rewrite(DefaultRule df) {
+		List<Clause> result = new ArrayList<Clause>();
+		Literal pre = df.getPrerequisite().get(0);
+		Literal conc = df.getConclusion().get(0);
+
+		final OWLPredicate concPredicate = conc.getPredicate().asOWLPredicate();
+		OWLPredicate concInPredicate = addPostfix(concPredicate, POSTFIX_IN);
+		OWLPredicate concOutPredicate = addPostfix(concPredicate, POSTFIX_OUT);
+		Literal concIn = new Literal(concInPredicate, conc.getTerms());
+		Literal concOut = new Literal(concOutPredicate, conc.getTerms());
+
+		Clause choiceRule1 = new Clause();
+		choiceRule1.setHead(concIn);
+//		choiceRule1.getPositiveBody().add;
+		choiceRule1.getNegativeBody().add(concOut);
+		result.add(choiceRule1);
+		
+		Clause choiceRule2 = new Clause();
+		choiceRule1.setHead(concOut);
+//		choiceRule1.getPositiveBody().add;
+		choiceRule1.getNegativeBody().add(concIn);
+		result.add(choiceRule2);
+		
+		
+		
+		return result;
+	}
+
+	protected OWLPredicate addPostfix(OWLPredicate p, String postfix) {
+		IRI iri = p.getLogicalEntity().getIRI();
+		IRI new_iri = IRI.create(iri.toString() + postfix);
+		OWLLogicalEntity e = null;
+		if (p.getArity() == 1) {
+			e = OWLManager.getOWLDataFactory().getOWLClass(new_iri);
+		} else if (p.getArity() == 2) {
+			e = OWLManager.getOWLDataFactory().getOWLObjectProperty(new_iri);
+		} else {
+			throw new IllegalStateException();
+		}
+		return new OWLPredicate(e);
+
+	}
+}
