c150e13142329f30bf25cc10b960054dd74a7ca2
xiao <xiao@b55cc70a-2b87-11e0-8e4b-6b21db92dc1d>
2012-11-12 18:17:46 +0000

    using maven assembly
    git-svn-id: https://www.kr.tuwien.ac.at/krrepos/xiao/imps/drew-el/trunk/drew-el@2786 b55cc70a-2b87-11e0-8e4b-6b21db92dc1d

---
+++ src/main/scripts/drew.el.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+
+
+if [ -n "${JAVA_HOME}" -a -x "${JAVA_HOME}/bin/java" ]; then
+  java="${JAVA_HOME}/bin/java"
+else
+  java=java
+fi
+
+if [ -z "${pellet_java_args}" ]; then
+  pellet_java_args="-Xmx512m"
+fi
+
+#dir=`dirname $0`            # The directory where the script is 
+#pushd "$dir" > /dev/null    # Go there
+#BASEDIR=$PWD             # Record the absolute path
+
+#BASEDIR=/Users/xiao/Dropbox/krrepos/xiao/imps/drew-el/trunk/drew-el/dist
+
+BASEDIR=/home/xiao/krrepos-xiao/imps/drew-el/trunk/drew-el/dist	
+
+#BASEDIR=${DREWELBASEDIR}
+
+#exec ${java} ${pellet_java_args} -jar lib/pellet-cli.jar "$@"
+exec ${java} -cp ${BASEDIR}/drew.el-0.0.1-SNAPSHOT.jar:${BASEDIR}/junit-4.8.2.jar:${BASEDIR}/owlapi-all-3.2.3.jar:${BASEDIR}/DLVwrapper-4.1.jar \
+ -DentityExpansionLimit=128000 org.semanticweb.drew.el.cli.DReWELCLI  "$@"
+#popd > /dev/null            # Return to previous dir
