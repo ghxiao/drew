3d5b1a95974a9f0c05ba9e73f01da5586eb9cf22
Guohui Xiao <gh.xiao@gmail.com>
2013-04-30 14:07:40 +0200

    add a sparql compiler test

---
+++ src/test/java/org/semanticweb/drew/rl/sparql/SparqlCompilerTest.java
@@ -0,0 +1,40 @@
+package org.semanticweb.drew.rl.sparql;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.semanticweb.drew.dlprogram.format.DLProgramStorer;
+import org.semanticweb.drew.dlprogram.format.DLProgramStorerImpl;
+import org.semanticweb.drew.dlprogram.format.RLProgramStorerImpl;
+import org.semanticweb.drew.dlprogram.model.Clause;
+import org.semanticweb.drew.el.reasoner.DReWELManager;
+import org.semanticweb.drew.el.reasoner.NamingStrategy;
+import org.semanticweb.drew.ldlp.reasoner.LDLPQueryCompiler;
+import org.semanticweb.drew.rl.sparql.SparqlCompiler;
+
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.Syntax;
+
+public class SparqlCompilerTest {
+
+    @Test
+    public void test() {
+        String prefixRDF = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ";
+        String queryText = prefixRDF + "PREFIX : <#>"
+                + "SELECT ?X ?Y " + "WHERE {"
+                + "?X rdf:type :preOnt1____ORDERS_LineItemQuantity ."
+                + "?X :preOnt1____hasValue ?Y " + "}";
+
+        Query query = QueryFactory.create(queryText, Syntax.syntaxARQ);
+        SparqlCompiler sparqlCompiler = new SparqlCompiler();
+
+        Clause drewQuery = sparqlCompiler.compileQuery(query);
+        LDLPQueryCompiler queryCompiler = new LDLPQueryCompiler();
+        Clause drewRLQuery = queryCompiler.compileQuery(drewQuery);
+        DLProgramStorer storer = new RLProgramStorerImpl();
+        storer.store(drewQuery, System.out);
+        storer.store(drewRLQuery, System.out);
+    }
+
+}
